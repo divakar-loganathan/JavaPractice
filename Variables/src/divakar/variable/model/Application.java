8cfb6c3f2ff195120e976eeab8449e9855ffc8bc
Divakar Loganathan <ldivakar@yahoo.co.in>
2018-02-08 01:57:27 -0600

    initial commit

---
+++ Variables/src/divakar/variable/model/Application.java
@@ -0,0 +1,25 @@
+package divakar.variable.model;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myNumber;
+		myNumber = 13;
+		System.out.println("My Number is : "+myNumber);
+	
+		short myShort = 123;
+		long myLong = 12345678;
+		
+		float myFloat =1.234f;
+		double myDouble = 123.45;
+		
+		char myChar ='D';
+		boolean myBoolean = true;
+		
+		byte myByte = 127;
+		
+		System.out.println(myShort + " " + myLong + " " + myFloat + " " + myDouble + " " + myChar + " " + myBoolean + " " + myByte);
+	}
+
+}
